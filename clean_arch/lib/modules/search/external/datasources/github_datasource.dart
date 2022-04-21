eabee7fd7ed30217a6d084997d8bb26730dc8ce1
Rafael Dias <rafael.dias@outlook.com>
2022-04-21 17:59:02 -0300

    External layer and tests

---
+++ clean_arch/lib/modules/search/external/datasources/github_datasource.dart
@@ -0,0 +1,27 @@
+import 'package:clean_arch/modules/search/data/datasource/search_datasource.dart';
+import 'package:clean_arch/modules/search/data/models/result_search_model.dart';
+import 'package:clean_arch/modules/search/domain/errors/errors.dart';
+import 'package:dio/dio.dart';
+
+extension on String {
+  normalize() {
+    return this.replaceAll(' ', '+');
+  }
+}
+
+class GithubDataSource implements SearchDatasource {
+  final Dio dio;
+
+  GithubDataSource(this.dio);
+
+  @override
+  Future<List<ResultSearchModel>?>? getSearch(String? searchText) async {
+    final response = await dio.get("https://api.github.com/search/users?q=${searchText ?? "".normalize()}");
+    if (response.statusCode == 200) {
+      final list = (response.data['items'] as List).map((e) => ResultSearchModel.fromMap(e)).toList();
+      return list;
+    } else {
+      throw DataSourceError();
+    }
+  }
+}
